e9251251f0fa7c50f3600a1404d3f65b60a87d9e
nitinbly84 <nitin.bly@gmail.com>
2020-12-30 21:11:20 +0530

    Creating the project

---
+++ GitProject/src/demo/Demo1.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author nitin
+ * @Date 30-Dec-2020 
+ */
+public class Demo1 {
+	public static void main(String... args) {
+		
+		System.out.println("This is creation of project in gmail git.....");
+	}
+}
