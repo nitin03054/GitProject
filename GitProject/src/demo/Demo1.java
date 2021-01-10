42ff407a763711f146fdf30b7e6d8b74e6302ff3
nitinbly84 <nitin.bly@gmail.com>
2021-01-10 10:34:46 +0530

    Edited by SME after fork

--- GitProject/src/demo/Demo1.java
+++ GitProject/src/demo/Demo1.java
@@ -10,5 +10,6 @@ public class Demo1 {
 		System.out.println("This is creation of project in gmail git.....");
 		System.out.println("Edited to check the PAT");
 		System.out.println("Edited by SME");
+		System.out.println("Edited by SME after fork");
 	}
 }
